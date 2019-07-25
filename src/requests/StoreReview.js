4cb511a28f9845c45eb49eddfebc85eed08f5836
James Kan <32029905+jameskanCa@users.noreply.github.com>
2019-07-24 20:39:19 -0400

    Removal of Firebase

--- src/requests/StoreReview.js
+++ src/requests/StoreReview.js
@@ -1,66 +1,68 @@
-import firebase from 'firebase';
-
 export class StoreReview {
-	static config = {
-		apiKey: 'AIzaSyD1X94iKdJNlMrjfnVGFneyad0qgMN5NDE',
-		authDomain: 'extensionproject.firebaseapp.com',
-		databaseURL: 'https://extensionproject.firebaseio.com/',
-		storageBucket: 'youtubeextensionproject.appspot.com'
-	};
-	static app = firebase.initializeApp(this.config);
-	static database = this.app.database();
+	// static config = {
+	// 	apiKey: 'AIzaSyD1X94iKdJNlMrjfnVGFneyad0qgMN5NDE',
+	// 	authDomain: 'extensionproject.firebaseapp.com',
+	// 	databaseURL: 'https://extensionproject.firebaseio.com/',
+	// 	storageBucket: 'youtubeextensionproject.appspot.com'
+	// };
+	// static app = firebase.initializeApp(this.config);
+	// static database = this.app.database();
 
 	static storeInitialReview(session) {
 		fetch('http://localhost:3001/test', {
-			method: 'post',
-			headers: {
-				'Content-Type': 'application/json',
-				Accept: '*',
-				'Access-Control-Request-Headers': '*',
-				'Access-Control-Request-Method': '*'
-			},
-			mode: 'cors',
-			body: JSON.stringify({
-				videoURL: session.videoURL,
-				purposeDescription: session.purposeDescription,
-				startTime: session.startTime,
-				finishedVideo: false
+				method: 'post',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: '*',
+					'Access-Control-Request-Headers': '*',
+					'Access-Control-Request-Method': '*'
+				},
+				mode: 'cors',
+				body: JSON.stringify({
+					videoURL: session.videoURL,
+					purposeDescription: session.purposeDescription,
+					startTime: session.startTime,
+					finishedVideo: false
+				})
 			})
-		})
-			.then(function(response) {
+			.then(function (response) {
 				console.log(response);
 			})
 			.catch((e) => {
 				console.log(e);
 			});
-		const dbRef = this.database.ref(session.userId);
-		const databasePushRef = dbRef.push({
-			videoURL: session.videoURL,
-			purposeDescription: session.purposeDescription,
-			startTime: session.startTime,
-			finishedVideo: false
-		});
-		console.log(databasePushRef);
-		return databasePushRef;
+		// const dbRef = this.database.ref(session.userId);
+		// const databasePushRef = dbRef.push({
+		// 	videoURL: session.videoURL,
+		// 	purposeDescription: session.purposeDescription,
+		// 	startTime: session.startTime,
+		// 	finishedVideo: false
+		// });
+		// console.log(databasePushRef);
+		// return databasePushRef;
 	}
 
-	static storeEndReview(endSession, databaseKey) {
-		this.database
-			.ref(`${endSession.userId}/${databaseKey}`)
-			.update({ endTime: endSession.endTime, notes: endSession.notes, finishedVideo: endSession.finishedVideo });
-	}
+	// static storeEndReview(endSession, databaseKey) {
+	// 	this.database
+	// 		.ref(`${endSession.userId}/${databaseKey}`)
+	// 		.update({
+	// 			endTime: endSession.endTime,
+	// 			notes: endSession.notes,
+	// 			finishedVideo: endSession.finishedVideo
+	// 		});
+	// }
 
-	static retrieveUserData(userId) {
-		console.log('called');
-		const dbRef = this.database.ref(`${userId}`);
-		return dbRef.on(
-			'value',
-			(data) => {
-				console.log(data.val());
-			},
-			(error) => {
-				console.log(error);
-			}
-		);
-	}
+	// static retrieveUserData(userId) {
+	// 	console.log('called');
+	// 	const dbRef = this.database.ref(`${userId}`);
+	// 	return dbRef.on(
+	// 		'value',
+	// 		(data) => {
+	// 			console.log(data.val());
+	// 		},
+	// 		(error) => {
+	// 			console.log(error);
+	// 		}
+	// 	);
+	// }
 }
